cb072db77e49e44c542b336149788eb3950b372d
Shariful Islam <engr.shariful.cse@gmail.com>
2023-11-17 23:09:31 +0100

    Searchable feature has been implemented

--- src/pages/Houses/Houses/AllHouses.jsx
+++ src/pages/Houses/Houses/AllHouses.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 
 const AllHouses = () => {
   const [houses, setHouses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredHouses = houses.filter((house) =>
+    house.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   useEffect(() => {
     const fetchHouses = async () => {
@@ -22,22 +27,48 @@ const AllHouses = () => {
   return (
     <div style={{ height: "800px" }}>
       <h1 className="text-5xl">All Houses will be shown here</h1>
-      <p>Total Houses: {houses.length}</p>
-      <div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 p-5 mt-5">
-        {houses.map((house) => (
-          <div
-            className="card bg-slate-200 p-5 font-semibold text-black"
-            key={house.id}
-          >
-            <Link to={`/houses/${house.slug}`}>
-              <h3>{house.name}</h3>
-            </Link>
-            {/* Additional house details can be added here */}
-          </div>
-        ))}
-      </div>
+      {/* <p>Total Houses: {houses.length}</p>
+
+      <h2>Houses Data</h2> */}
+      <input
+        type="text"
+        placeholder="Search houses..."
+        className="input input-bordered input-info w-full max-w-xs mt-6"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ul className="grid gap-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 p-5 mt-5">
+        {filteredHouses.length > 0 ? (
+          filteredHouses.map((house) => (
+            <li
+              className="card bg-slate-200 p-5 font-semibold text-black"
+              key={house.slug}
+            >
+              <Link to={`/houses/${house.slug}`}>
+                <h3>{house.name}</h3>
+              </Link>
+              {/* Additional house details can be added here */}
+            </li>
+          ))
+        ) : (
+          <p>No houses found for the current search term.</p>
+        )}
+      </ul>
     </div>
   );
 };
 
 export default AllHouses;
+// {
+//   houses.map((house) => (
+//     <div
+//       className="card bg-slate-200 p-5 font-semibold text-black"
+//       key={house.id}
+//     >
+//       <Link to={`/houses/${house.slug}`}>
+//         <h3>{house.name}</h3>
+//       </Link>
+//       {/* Additional house details can be added here */}
+//     </div>
+//   ));
+// }
--- src/pages/Persons/Persons/Persons.jsx
+++ src/pages/Persons/Persons/Persons.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Persons = () => {
+  const [persons, setPersons] = useState([]);
 
+  useEffect(() => {
+    const fetchPersons = async () => {
+      try {
+        const response = await fetch(
+          "https://api.gameofthronesquotes.xyz/v1/characters"
+        );
+        const result = await response.json();
+        setPersons(result);
+      } catch (error) {
+        console.error("Error fetching persons:", error);
+      }
+    };
 
-  
+    fetchPersons();
+  }, []);
+
   return (
     <div style={{ height: "800px" }}>
       <h1 className="text-5xl">All Persons will be displayed here</h1>
 
-
-
+      <div>
+        <h2>Persons</h2>
+        {persons.map((person) => (
+          <Link key={person.slug} to={`/persons/${person.slug}`}>
+            <div className="list-item">
+              <h3>{person.name}</h3>
+              {person.house && <p>House: {person.house}</p>}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
